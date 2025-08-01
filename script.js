1eee3c1a03eb44ba13d32f938165a6ec3ff27dcd
Faiz <mohdfaiz182005@gmail.com>
2025-08-01 15:50:38 +0530

    added functionality

---
+++ script.js
@@ -0,0 +1,57 @@
+let playerX = document.getElementById("x");
+let playerO = document.getElementById("o");
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let fillCount = 0;
+
+let inputs = document.querySelectorAll(".game-center input");
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    let symbol;
+    if (playerX.hasAttribute("checked")) {
+      input.value = "X";
+      symbol = "X";
+      playerX.removeAttribute("checked");
+      playerO.setAttribute("checked", "checked");
+      fillCount += 1;
+    } else {
+      input.value = "O";
+      symbol = "O";
+      playerO.removeAttribute("checked");
+      playerX.setAttribute("checked", "checked");
+      fillCount += 1;
+    }
+
+    input.style.backgroundColor = "whitesmoke";
+    input.setAttribute("disabled", "disabled");
+
+    winPatterns.forEach((pattern) => {
+      const [a, b, c] = pattern;
+
+      if (
+        inputs[a].value === symbol &&
+        inputs[b].value === symbol &&
+        inputs[c].value === symbol
+      ) {
+        console.log(`Player ${symbol} wins`);
+        inputs.forEach((input) => {
+          input.setAttribute("disabled", "disabled");
+        });
+      }
+    });
+
+    if (fillCount === 9) {
+      console.log("Its a draw");
+    }
+  });
+});
